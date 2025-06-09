41038c1a2dec6cf0ef6f5a52f6c1923d4287dda5
Anish-2005 <anishseth0510@gmail.com>
2025-06-10 03:04:20 +0530

    +

--- coc/app/layout.jsx
+++ coc/app/layout.jsx
@@ -7,7 +7,7 @@ export const metadata = {
   title: "Clash of Clans",
   description: "Answer the call of the mustache! Join the international fray that is Clash of Clans. Customize your village, build an army and crush your opponents. Like using friendship to strike fear into your enemies? Join a Clan, or establish a Clashing legacy by creating your own. The choice is yours in this millions-strong community of Barbarians.",
   icons: {
-    icon: "/public/castle.png",
+    icon: "/icons/castle.png",
   },
 };
 
